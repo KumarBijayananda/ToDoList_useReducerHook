cf68d7c7c6bd686a3efca7e587344792a2624318
Kumar Bijayananda <kumarbijayananda@gmail.com>
2025-02-14 23:48:06 -0500

    css and documentation

--- src/App.jsx
+++ src/App.jsx
@@ -2,38 +2,46 @@ import { useReducer, useState } from "react";
 import "./App.css";
 
 function App() {
+
+  //reducer function to handle actions
   function TodoReducer(state, { type, payload }) {
     switch (type) {
+      //adding a new item to the list
       case "add":
         return [
           {
-            id: Date.now(),
-            item: payload,
-            isComplete: false,
-            isEditing: false,
+            id: Date.now(),   //using predefined Date function to create an unique id
+            item: payload,    //to do item
+            isComplete: false,  //boolean variable to check if complete
+            isEditing: false,   //boolean variable to check if item is in edit mode
           },
           ...state,
         ];
+      //action for save button for items in edit mode
       case "save":
         return state.map((todo) =>
-          todo.id === payload.id
+          todo.id === payload.id    
             ? { ...todo, item: payload.newItem, isEditing: false }
             : todo
         );
+      //action for delete button to delete an item
       case "delete":
         return state.filter((item) => item.id !== payload.id);
+      //toggle action for checkbox
       case "toggle":
         return state.map((todo) =>
           todo.id === payload.id
             ? { ...todo, isComplete: !todo.isComplete }
             : todo
         );
+      //action for item to be put in edit mode
       case "setEdit":
         return state.map((todo) =>
           todo.id === payload.id
             ? { ...todo, isEditing: true }
             : { ...todo, isEditing: false }
         );
+      //action for cancel button  
       case "cancelEdit":
         return state.map((todo) =>
           todo.id === payload.id ? { ...todo, isEditing: false } : todo
@@ -43,10 +51,11 @@ function App() {
     }
   }
 
-  const [todo, dispatchTodo] = useReducer(TodoReducer, []);
-  const [form, setForm] = useState("");
-  const [editText, setEditText] = useState("");
+  const [todo, dispatchTodo] = useReducer(TodoReducer, []); //useReducer for different actions
+  const [form, setForm] = useState("");     //useState to handle form input
+  const [editText, setEditText] = useState(""); //useState to handle edit mode
 
+  
   function handleChange(event) {
     setForm({ ...form, [event.target.name]: event.target.value.trim() });
   }
@@ -77,7 +86,7 @@ function App() {
       <h1>To-do List</h1>
       <form className="mainContainer" onSubmit={handleSubmit}>
         <input type="text" onChange={handleChange} name="newItem" placeholder="Add your item" />
-        <button type="submit">Add</button>
+        <button type="submit" className="addBtn">Add</button>
       </form>
 
       <ul>
